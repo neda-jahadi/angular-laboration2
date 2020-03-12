55c6b99f1e2e9406b8180dfb02e1f31f898ab101
Neda Jahadi <neda_jahadi@yahoo.com>
2020-03-12 12:40:53 +0100

    changes in startsidan css and html

--- src/app/form-service.service.ts
+++ src/app/form-service.service.ts
@@ -6,13 +6,21 @@ import { ArtikleInfo} from './artikle-info';
 export class FormServiceService {
   
   artikleList: ArtikleInfo[] = [
-		{ title: 'title1', content:'content1', time: 1, author:'author1' },
-		{ title: 'title2', content:'content2', time: 2, author:'author2' },
-		{ title: 'title3', content:'content3', time: 3, author:'author3' },
-    { title: 'title4', content:'content4', time: 4, author:'author4' },
-    { title: 'title5', content:'content5', time: 5, author:'author5' },
-    { title: 'title6', content:'content6', time: 6, author:'author6' },
-    { title: 'title7', content:'content7', time: 7, author:'author7' }
+    { title: 'Robots in the bedroom',
+     content:'Understanding cloud technology was the No. 1 imperative for executives this decade. Next up: How does artificial intelligence change business and management?', 
+     time: 20, author:'Sean Silverthorne' },
+    { title: 'Use artificial inteligence', content:'Setting sales targets has always been an inexact science, with serious consequences if done poorly. Using AI-based advanced analytics might be the answer, argues Doug Chung.',
+     time: 15, author:'Michael Blanding' },
+    { title: 'Making technical hire', content:'CEOs are usually more comfortable making key hires on the business side of the house than the technology side. Here is what executives need to understand about technical hires.',
+     time: 17, author:'Julia Austin' },
+    { title: 'Going digital', content:'More and more nontechnology companies are adopting digital technologies like AI, data analytics, and machine learning.',
+     time: 31, author:'Wilbur Chen' },
+    { title: 'Two decates later', content:'SUMMING UP Have recent advances in technology such as artificial intelligence ushered in (finally) the "new economy?" Not so fast, answer James Heskett.',
+     time: 24, author:'James Heskett' },
+    { title: 'Componies like big data', content:'Big data is a critical competitive advantage for companies that know how to use it. Harvard Business School faculty share insights that they teach to executives.', 
+    time: 11, author:'Dina Gerdeman' },
+    { title: 'The promises and limitations', content:'Recent stories chart the rise of big data, its impact on business, and how it affects our lives every day.', 
+    time: 21, author:'Sean Silverthorne' }
   ];
   listLength: number = this.artikleList.length;
   senasteArtiklar: ArtikleInfo[]=[];
