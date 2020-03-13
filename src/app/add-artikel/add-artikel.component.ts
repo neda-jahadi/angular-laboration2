cf24fc76d32968b941d16bfd2b245ff66ce397b5
Neda Jahadi <neda_jahadi@yahoo.com>
2020-03-13 08:47:07 +0100

    submit button in center, change default article

--- src/app/add-artikel/add-artikel.component.ts
+++ src/app/add-artikel/add-artikel.component.ts
@@ -15,7 +15,7 @@ export class AddArtikelComponent implements OnInit {
   constructor(public formService: FormServiceService) { }
 
   ngOnInit(): void {
-    this.model = new ArtikleInfo('ARDS strategy tested', 'On January 28, three additional employees at the company tested positive for 2019-nCoV (Patients 2 through 4 in Figure 1). Of these patients, only Patient 2 had contact with the index patient; ', 
+    this.model = new ArtikleInfo('ARDS strategy tested', 'On January 28, three additional employees at the company tested positive for 2019-nCoV (Patients 2 through 4 in Figure 1). Of these patients, only Patient 2 had contact with the index patient; Distinctions between cell types underpin organizational principles for nervous system function. Functional variation also exists between neurons of the same type. This is exemplified by correspondence between grid cell spatial scales and the synaptic integrative properties of stellate cells (SCs) in the medial entorhinal cortex.', 
     20, 'L.Barrot');
     console.log(this.model);
   }
