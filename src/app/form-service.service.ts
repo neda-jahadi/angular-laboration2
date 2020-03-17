51aec72d3bdd9e04c08914f4d6e42362440a4098
Andreas Karlsson <andreas.karlsson90@hotmail.com>
2020-03-17 12:56:06 +0100

    senaste artiklar

--- src/app/form-service.service.ts
+++ src/app/form-service.service.ts
@@ -4,10 +4,10 @@ import { ArtikleInfo} from './artikle-info';
   providedIn: 'root'
 })
 export class FormServiceService {
-  
+
   artikleList: ArtikleInfo[] = [
     { title: 'Robots in the bedroom',
-     content:'Understanding cloud technology was the No. 1 imperative for executives this decade. Next up: How does artificial intelligence change business and management?', 
+     content:'Understanding cloud technology was the No. 1 imperative for executives this decade. Next up: How does artificial intelligence change business and management?',
      time: 20, author:'Sean Silverthorne' },
     { title: 'Use artificial inteligence', content:'Setting sales targets has always been an inexact science, with serious consequences if done poorly. Using AI-based advanced analytics might be the answer, argues Doug Chung.',
      time: 15, author:'Michael Blanding' },
@@ -17,32 +17,45 @@ export class FormServiceService {
      time: 31, author:'Wilbur Chen' },
     { title: 'Two decates later', content:'SUMMING UP Have recent advances in technology such as artificial intelligence ushered in (finally) the "new economy?" Not so fast, answer James Heskett.',
      time: 24, author:'James Heskett' },
-    { title: 'Componies like big data', content:'Big data is a critical competitive advantage for companies that know how to use it. Harvard Business School faculty share insights that they teach to executives.', 
+    { title: 'Componies like big data', content:'Big data is a critical competitive advantage for companies that know how to use it. Harvard Business School faculty share insights that they teach to executives.',
     time: 11, author:'Dina Gerdeman' },
-    { title: 'The promises and limitations', content:'Recent stories chart the rise of big data, its impact on business, and how it affects our lives every day.', 
+    { title: 'The promises and limitations', content:'Recent stories chart the rise of big data, its impact on business, and how it affects our lives every day.',
     time: 21, author:'Sean Silverthorne' }
   ];
   listLength: number = this.artikleList.length;
-  senasteArtiklar: ArtikleInfo[]=[];
-  getSenaste(): ArtikleInfo[] {
-    this.senasteArtiklar=[];
-    for(let i=0; i<5; i++){this.senasteArtiklar.push(this.artikleList[i])}
-    return this.senasteArtiklar;
+  // senasteArtiklar: ArtikleInfo[]=[];
+	getSenaste(): ArtikleInfo[]
+	{
+		let senasteArtiklar=[];
+		for(let i=0; i<5; i++)
+		{
+			senasteArtiklar.push(this.artikleList[i])
+		}
+		senasteArtiklar = senasteArtiklar.map(article => ({
+			title: article.title,
+			content: article.content.substring(0, 30),
+			author: article.author,
+			time: article.time
+		}));
+		console.log(senasteArtiklar);
+		return senasteArtiklar;
+	}
+
+  getArtikleList(): ArtikleInfo[]
+  {
+	  return this.artikleList;
   }
-  getArtikleList(): ArtikleInfo[] {
-		return this.artikleList;
+
+  addNewArticle(element: any): void
+  {
+	  this.artikleList.unshift(element);
   }
-  
-  addNewArticle(element: any): void {
-    
-    
-    this.artikleList.unshift(element);
-    
+
+  deleteArticle(article: object)
+  {
+	  this.artikleList=this.artikleList.filter((ele)=>{return ele!=article});
   }
-  
-  deleteArticle(article: object){
-     this.artikleList=this.artikleList.filter((ele)=>{return ele!=article});
-     
-  }
-  constructor() { }
+
+  constructor()
+  {}
 }
